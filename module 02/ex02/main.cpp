e10dae746f9366118bf7ff4b40f25cd8da978c1d
Mohammed El Kouche <mel-kouc@e3r2p9.1337.ma>
2023-11-30 10:24:12 +0100

    add folder module 03

--- module 02/ex02/main.cpp
+++ module 02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mel-kouc <mel-kouc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 11:58:29 by mel-kouc          #+#    #+#             */
-/*   Updated: 2023/11/28 01:40:37 by mel-kouc         ###   ########.fr       */
+/*   Updated: 2023/11/30 10:22:22 by mel-kouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,14 @@ int main( void )
     std::cout << a++ << std::endl;
     std::cout << a << std::endl;
     std::cout << b << std::endl;
+    Fixed c;
+    c = a + b;
+    
+    std::cout << c << std::endl;
+    if (c <b )
+            std::cout << "b is gerat" << std::endl;
+    else
+        std::cout << "b is small" << std::endl;
     std::cout << Fixed::max( a, b ) << std::endl;
     return 0;
 }
